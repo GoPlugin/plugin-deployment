d1c99f7b43b84594e52bbf1c289d57dd5846a711
nmzn <95173225+nmzn@users.noreply.github.com>
2022-02-11 09:45:30 +0100

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -6,7 +6,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
-sudo systemctl status docker
-sleep 3
 echo
 echo -e "### DOCKER INSTALLED ###\n"
