d594979965275c2479bc7bb6350a82d6a07a3c87
Lokesh <lokeshwaran.a82@gmail.com>
2022-02-11 12:51:46 +0400

    Revert "updated to my docker installer"

--- README.md
+++ README.md
@@ -1,40 +1,68 @@
-# pluginnode-install
+<br/>
+<p align="center">
+<a href="https://goplugin.co" target="_blank">
+<img src="https://github.com/GoPlugin/Plugin/blob/main/docs/plugin.png" width="225" alt="Plugin logo">
+</a>
+</p>
+<br/>
 
-# Please take note that the "./install_node.sh" does not include the docker installation. If you have no Docker installed on your VPS you need to run the "./install_docker.sh" first
+This repo contains the script for automation of the [Plugin](https://goplugin.co/) Node setup which can be used by the Node operators.
 
-Guide on how to deploy Contracts: https://github.com/nmzn/deployment-guide
+To follow [Mr.Blochain22](https://www.youtube.com/watch?v=GEp_OmT-WwY&t=196s) video tutorial:
+```
+Steps to be followed:
+=====================
+1) git clone https://github.com/GoPlugin/plugin-deployment.git
+2) cd plugin-deployment
+3) git checkout plugin-dep-mb22
+```
 
-The install Script hasnt a password check yet. Will include that in later updates.
+To install latest scripts:
+==========================
+```
+Steps to be followed:
+=====================
+1) git clone https://github.com/GoPlugin/plugin-deployment.git
+2) cd plugin-deployment
+```
+There are 2 scripts in this repo(1_prerequisite.bash,2_nodeStartPM2.sh) & 2 files (apicredentials.txt, password.txt). 
 
-Contents:
+1) Before executing the 1_prerequisite.bash script, please change the database password
+ to your own known password(we just given a default as 'postgres', just change 'your_password') at line number 202 of 1_prerequisite.bash.
+This script will install all the prerequisite tools, utilities for Plugin.
+- After successfull execution of 1_prerequisite.bash, kindly go through the instructions to be followed
+  for executing 2_nodeStartPM2.sh as mentioned below.
+```
+      ################################################################################
+      # 			IMPORTANT MESSAGE                                    #
+      ################################################################################
+      # Make sure you have the below mentioned 2  files are available and populated  #
+      # as given below. Then start 'pm2 start 2_nodeStartPM2.sh' script to run your#
+      # node in the background. To view your node log use 'pm2 logs 0'.              #
+      #                                                                              #
+      # File 1: password.txt => contains your keystore password                      #
+      #           *** KEYSTORE PASSWORD SHOULD FOLLOW THIS CONDITIONS ***	     #
+      #                   “must be longer than 12 characters”,			     #
+      #			  “must contain at least 3 lowercase characters”,	     #
+      # 		  “must contain at least 3 uppercase characters”,	     #
+      #			  “must contain at least 3 numbers”,			     #
+      #			  “must contain at least 3 symbols”,			     #
+      # 		  “must not contain more than 3 identical consecutive 	     #
+      #     		   characters”.						     #
+      # File 2: apicredentials.txt => first line of the file contians email id for UI#
+      #                              second line of the file contains password for UI#
+      #				     (This passwrod should be strong, but need not   #
+      #				      follow keystore password condition).	     #
+      #										     #	
+      # NOTE: This 2 files have default contents, please change the mail & passwords #
+      #	      before starting 'pm2 start 2_nodeStartPM2.sh'.			     #
+      ################################################################################
+      ################################################################################
+```
+2) Now execute 2_nodeStartPM2.sh through pm2 'pm2 start 2_nodeStartPM2.sh'.
+3) You can view the status(pm2 status 0) && logs(pm2 logs 0) of your running node
 
-  ##install_docker.sh
 
-    This is a small Script that installs docker for you.
-  
-  ##install_node.sh
-  
-    This is the main Install Script that will install the plugin Node including the external initiator.
-      
-    
-  Step 1:
-      
-          
-    sudo git clone https://github.com/nmzn/pluginnode-install.git && cd pluginnode-install && sudo chmod +x install_docker.sh && sudo chmod +x install_node.sh
-      
-  
-  Step 2 (optional if needed):
-      
-    ./install_docker.sh
-  
-  Step 3:
-  
-    ./install_node.sh
-          
-    
-   #This does not include any changings to your Firewall you may need to apply in order for the node to run correctly. 
-    
-   #Important
-   
-   Keep in mind that you still have to do the Oracle Contract deployment part wich you find here: https://docs.goplugin.co
-   
+Please follow the link [Node setup Guide](https://medium.com/@GoPlugin/setup-a-plugin-node-automated-way-using-shell-script-fbdec48a0dea) on medium for detailed process steps.
+
+To withdraw your XDC/PLI from your node please refer to this article (https://medium.com/@GoPlugin/how-to-withdraw-xdc-pli-from-plugin-node-oracle-address-a9ebe6ff2dd7).
---
+++ _pli_node_deploy.sh
@@ -0,0 +1,438 @@
+#!/bin/bash
+
+# Authenticate sudo perms before script execution to avoid timeouts or errors
+sudo -l > /dev/null 2>&1
+
+# Set Colour Vars
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+
+FUNC_VARS(){
+## VARIABLE / PARAMETER DEFINITIONS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Get current user id and store as var
+    USER_ID=$(getent passwd $EUID | cut -d: -f1)
+
+
+    PLI_VARS_FILE="plinode_$(hostname -f)".vars
+    if [ ! -e ~/$PLI_VARS_FILE ]; then
+        clear
+        echo
+        echo
+        echo -e "${RED} #### ERROR: No VARIABLES file found. ####${NC}"
+        echo
+        echo -e "${RED} ..creating local vars file '$HOME/$PLI_VARS_FILE' ${NC}"
+        cp sample.vars ~/$PLI_VARS_FILE
+        echo
+        echo -e "${GREEN} please update the vars file with your specific values.. ${NC}"
+        echo -e "${GREEN} copy command to edit: ${NC}"
+        echo
+        echo -e "${GREEN}nano ~/$PLI_VARS_FILE ${NC}"
+        echo
+        echo
+        #sleep 2s
+        exit 1
+    fi
+    source ~/$PLI_VARS_FILE
+
+}
+
+
+
+FUNC_VALUE_CHECK(){
+    echo -e "${GREEN}#########################################################################"
+    echo -e "${GREEN}#########################################################################"
+    echo -e "${GREEN}"
+    echo -e "${GREEN}     Script Deployment menthod"
+    echo -e "${GREEN}"
+    echo -e "${GREEN}#########################################################################"
+    echo -e "${GREEN}#########################################################################${NC}"
+
+
+
+    echo -e "${GREEN}#########################################################################"
+    echo
+    echo -e "${GREEN}## CONFIRM SCRIPTS VARIABLES FILE HAS BEEN UPDATED...${NC}"
+    echo 
+    # Ask the user acc for login details (comment out to disable)
+    
+        while true; do
+            read -t7 -r -p "please confirm that you have updated the vars file with your values ? (Y/n) " _input
+            if [ $? -gt 128 ]; then
+                clear
+                echo
+                echo "timed out waiting for user response - proceeding as normal..."
+                FUNC_NODE_DEPLOY;
+            fi
+            case $_input in
+                [Yy][Ee][Ss]|[Yy]* ) 
+                    FUNC_NODE_DEPLOY
+                    break
+                    ;;
+                [Nn][Oo]|[Nn]* ) 
+                    FUNC_EXIT
+                    ;;
+                * ) echo "Please answer (y)es or (n)o.";;
+            esac
+        done
+}
+
+
+
+FUNC_NODE_DEPLOY(){
+    FUNC_VARS;
+    
+    echo -e "${GREEN}#########################################################################"
+    echo -e "${GREEN}#########################################################################"
+    echo -e "${GREEN}"
+    echo -e "${GREEN}     Script Deployment menthod"
+    echo -e "${GREEN}"
+    echo -e "${GREEN}#########################################################################"
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo 
+    echo 
+
+    echo -e "${GREEN}#########################################################################"
+    echo
+    echo -e "${GREEN}## Install: Credentials files prep...${NC}"
+    echo 
+    
+    touch {$FILE_KEYSTORE,$FILE_API}
+    chmod 666 {$FILE_KEYSTORE,$FILE_API}
+
+    echo $API_EMAIL > $FILE_API
+    echo $API_PASS >> $FILE_API
+    echo $PASS_KEYSTORE > $FILE_KEYSTORE
+
+    chmod 600 {$FILE_KEYSTORE,$FILE_API}
+
+    # Remove the file if necessary; sudo rm -f {.env.apicred,.env.password}
+
+
+
+    echo 
+    echo 
+
+    echo -e "${GREEN}#########################################################################"
+    echo
+    echo -e "${GREEN}## Install: UPDATE bash file $BASH_FILE1 with user values...${NC}"
+    echo 
+
+    sed -i.bak "s/$DB_PWD_FIND/'$DB_PWD_NEW'/g" $BASH_FILE1
+    #cat $BASH_FILE1 | grep 'postgres PASSWORD'
+    #sleep 1s
+
+
+    echo 
+    echo 
+
+    echo -e "${GREEN}#########################################################################"
+    echo
+    echo -e "${GREEN}## Install: PRE-CHECKS for bash file $BASH_FILE1...${NC}"
+    echo 
+
+    sudo apt remove --autoremove golang -y
+    sudo rm -rf /usr/local/go
+
+
+    echo 
+    echo 
+
+    echo -e "${GREEN}#########################################################################"
+    echo
+    echo -e "${GREEN}## Install: EXECUTE bash file $BASH_FILE1...${NC}"
+    echo 
+
+    bash $BASH_FILE1
+
+    echo 
+    echo 
+
+    echo -e "${GREEN}#########################################################################"
+    echo
+    echo -e "${GREEN}## Install: Update bash file $BASH_FILE2 with user CREDENTIALS values...${NC}"
+    echo 
+
+    sed -i.bak "s/password.txt/$FILE_KEYSTORE/g" $BASH_FILE2
+    sed -i.bak "s/apicredentials.txt/$FILE_API/g" $BASH_FILE2
+    sed -i.bak "s/:postgres/:$DB_PWD_NEW/g" $BASH_FILE2
+    sed -i.bak '/SECURE_COOKIES=false/d' $BASH_FILE2
+    cat $BASH_FILE2 | grep node
+    sleep 1s
+
+
+    echo 
+    echo 
+    echo -e "${GREEN}## Install: Update bash file $BASH_FILE2 with user TLS values...${NC}"
+    echo 
+
+    sed -i.bak "s/PLUGIN_TLS_PORT=0/PLUGIN_TLS_PORT=$PLI_HTTPS_PORT/g" $BASH_FILE2
+    sed -i.bak "/^export PLUGIN_TLS_PORT=.*/a export TLS_CERT_PATH=$TLS_CERT_PATH/server.crt\nexport TLS_KEY_PATH=$TLS_CERT_PATH/server.key" $BASH_FILE2
+    cat $BASH_FILE2 | grep TLS
+    sleep 1s
+
+
+    echo 
+    echo 
+    echo -e "${GREEN}## Install: Create loca Certificate Authority / TLS Certificate & files / folders...${NC}"
+    echo 
+
+    
+    mkdir $TLS_CERT_PATH && cd $TLS_CERT_PATH
+    openssl req -x509 -out server.crt -keyout server.key -newkey rsa:4096 \
+-sha256 -days 3650 -nodes -extensions EXT -config \
+<(echo "[dn]"; echo CN=localhost; echo "[req]"; echo distinguished_name=dn; echo "[EXT]"; echo subjectAltName=DNS:localhost; echo keyUsage=digitalSignature; echo \
+extendedKeyUsage=serverAuth) -subj "/CN=localhost"
+    sleep 1s
+
+
+
+    echo 
+    echo 
+    echo -e "${GREEN}## Install: Update bash file $BASH_FILE2 with INITIATORS values...${NC}"
+    echo 
+    sed -i.bak "/^ export DATABASE_TIMEOUT=.*/a export FEATURE_EXTERNAL_INITIATORS=true" $BASH_FILE2
+    cat $BASH_FILE2 | grep INITIATORS
+    sleep 1s
+
+
+    echo -e "${GREEN}## Install: Check Golang version & bash profile path...${NC}"
+    echo 
+    source ~/.profile
+    GO_VER=$(go version)
+    go version; GO_EC=$?
+    case $GO_EC in
+        0) echo -e "${GREEN}## Command exited with NO error...${NC}"
+            echo $GO_VER
+            echo
+            echo -e "${GREEN}## Install proceeding as normal...${NC}"
+            ;;
+        1) echo -e "${RED}## Command exited with ERROR - updating bash profile...${NC}"
+            echo
+            source ~/.profile;
+            sudo sh -c 'echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile'
+            echo "cat "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile"
+            echo
+            echo -e "${RED}## Check GO Version manually...${NC}"
+            sleep 2s
+            #FUNC_EXIT_ERROR
+            #exit 1
+            ;;
+        *) echo -e "${RED}## Command exited with OTHER ERROR...${NC}"
+            echo -e "${RED}## 'go version' returned : $GO_EC ${NC}"
+            echo
+            FUNC_EXIT_ERROR
+            #exit 1
+            ;;
+    esac
+
+    sleep 1s
+
+
+    echo -e "${GREEN}## Install: Start PM2 $BASH_FILE2 & set auto start on reboot...${NC}"
+    echo 
+    cd /$PLI_DEPLOY_PATH
+    pm2 start $BASH_FILE2
+    sleep 1s
+    pm2 list 
+    
+    sleep 2s
+    pm2 list
+    pm2 startup systemd
+    sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER_ID --hp /home/$USER_ID
+    pm2 save
+
+    # INTERACTIVE: Calls function to check if user wants to proceed to next stage of setup.
+    #FUNC_DO_INIT_CHECK;
+
+    # NON-INTERACTIVE: Proceed with next stage of setup.
+    FUNC_INITIATOR;
+    }
+
+
+
+
+
+
+
+
+FUNC_INITIATOR(){
+    FUNC_VARS;
+
+
+    
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo
+    echo -e "${GREEN}## CLONE & INSTALL LOCAL INITIATOR...${NC}"
+    echo 
+
+    # Added to resolve error running 'plugin help'
+    source ~/.profile
+    
+    cd /$PLI_DEPLOY_PATH
+    git clone https://github.com/GoPlugin/external-Initiator
+    cd $PLI_INITOR_DIR
+    git checkout main
+    go install
+
+
+
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo
+    echo -e "${GREEN}## CREATE LOCAL INITIATOR...${NC}"
+    echo 
+    export FEATURE_EXTERNAL_INITIATORS=true
+    plugin admin login -f "../$FILE_API"
+    sleep 0.5s
+    plugin initiators create $PLI_L_INIT_NAME http://localhost:8080/jobs > $PLI_INIT_RAWFILE
+
+    # plugin initiators create xdc http://localhost:8080/jobs
+    # plugin initiators destroy xdc http://localhost:8080/jobs
+    # plugin initiators create xdc http://localhost:8080/jobs > $PLI_INIT_RAWFILE
+
+
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo
+    echo -e "${GREEN}## CAPTURE INITIATOR CREDENTIALS & FILE MANIPULATION...${NC}"
+    echo 
+    sed -i 's/ ║ /,/g;s/╬//g;s/═//g;s/║//g' $PLI_INIT_RAWFILE
+    sed -n '/'"$PLI_L_INIT_NAME"'/,//p' $PLI_INIT_RAWFILE > $PLI_INIT_DATFILE
+    sed -i 's/,/\n/g;s/^.'"$PLI_L_INIT_NAME"'//g' $PLI_INIT_DATFILE
+    sed -i 's/^http.*//g' $PLI_INIT_DATFILE
+    sed -i.bak '/^$/d;/^\s*$/d;s/[ \t]\+$//' $PLI_INIT_DATFILE
+    cp $PLI_INIT_DATFILE ~/$PLI_INIT_DATFILE.bak  && chmod 600 ~/$PLI_INIT_DATFILE.bak
+    cat $PLI_INIT_DATFILE
+    sleep 1s
+
+
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo
+    echo -e "${GREEN}## READ INITIATOR CREDENTIALS AS VARIABLES...${NC}"
+    echo 
+    read -r -d '' EXT_ACCESSKEY EXT_SECRET EXT_OUTGOINGTOKEN EXT_OUTGOINGSECRET <$PLI_INIT_DATFILE
+    echo
+    #echo "$EXT_ACCESSKEY"
+    #echo "$EXT_SECRET"
+    #echo "$EXT_OUTGOINGTOKEN"
+    #echo "$EXT_OUTGOINGSECRET"
+    sleep 1s
+
+
+    echo
+    echo
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo
+    echo -e "${GREEN}## CREATE INITIATOR PM2 SERVICE FILE: $BASH_FILE3 & file perms ${NC}"
+    echo
+    cd /$PLI_DEPLOY_PATH
+    cat <<EOF > $BASH_FILE3
+#!/bin/bash
+export EI_DATABASEURL=postgresql://postgres:${DB_PWD_NEW}@127.0.0.1:5432/plugin_mainnet_db?sslmode=disable
+export EI_CHAINLINKURL=http://localhost:6688
+export EI_IC_ACCESSKEY=${EXT_ACCESSKEY}
+export EI_IC_SECRET=${EXT_SECRET}
+export EI_CI_ACCESSKEY=${EXT_OUTGOINGTOKEN}
+export EI_CI_SECRET=${EXT_OUTGOINGSECRET}
+echo *** Starting EXTERNAL INITIATOR ***
+external-initiator "{\"name\":\"$PLI_E_INIT_NAME\",\"type\":\"xinfin\",\"url\":\"https://pluginrpc.blocksscan.io\"}" --chainlinkurl "http://localhost:6688/"
+EOF
+    sleep 1s
+    cat $BASH_FILE3
+    chmod u+x $BASH_FILE3
+
+
+    echo 
+    echo 
+    echo -e "${GREEN}#########################################################################${NC}"
+    echo
+    echo -e "${GREEN}## START INITIATOR PM2 SERVICE $BASH_FILE3 ${NC}"
+    echo    
+    pm2 start $BASH_FILE3
+    sleep 1s
+    pm2 status
+    sleep 3s
+    pm2 startup systemd
+    sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER_ID --hp /home/$USER_ID
+    pm2 save
+
+    FUNC_EXIT;
+}
+
+
+
+
+
+
+FUNC_DO_INIT_CHECK(){
+
+    echo -e "${GREEN}#########################################################################"
+    echo
+    echo -e "${GREEN}## CONFIRM SCRIPTS EXPORT VALUES HAVE BEEN UPDATED...${NC}"
+    echo 
+    
+        while true; do
+            read -t10 -r -p "Do you wish to proceed to INITIATOR SETUP ? (Y/n) " _input
+            if [ $? -gt 128 ]; then
+                #clear
+                echo
+                echo "timed out waiting for user response - proceeding as normal..."
+                FUNC_INITIATOR;
+            fi
+            case $_input in
+                [Yy][Ee][Ss]|[Yy]* ) 
+                    FUNC_INITIATOR
+                    break
+                    ;;
+                [Nn][Oo]|[Nn]* ) 
+                    FUNC_EXIT
+                    ;;
+                * ) echo "Please answer (y)es or (n)o."
+                    echo "NOTE: timeout value is (y)es";;
+            esac
+        done
+}
+
+
+
+
+FUNC_EXIT(){
+	exit 0
+	}
+
+
+
+FUNC_EXIT_ERROR(){
+	exit 1
+	}
+  
+#FUNC_VALUE_CHECK;
+
+clear
+case "$1" in
+        fullnode)
+                FUNC_VALUE_CHECK
+                ;;
+        initiator)
+                FUNC_INITIATOR
+                ;;
+        *)
+                
+                echo 
+                echo 
+                echo "Usage: $0 {fullnode|initiator}"
+                echo 
+                echo "please provide one of the above values to run the scripts"
+                echo "    example: " $0 fullnode""
+                echo 
+                echo 
+esac
