6619bb01c94464e978f0256ed1090d1b83a1281e
nmzn <95173225+nmzn@users.noreply.github.com>
2022-04-09 14:52:21 +0200

    updated with autostart service creation
    script will now create a service which runs on startup to run the node and ei

--- oneClickDeploy/install_node.sh
+++ oneClickDeploy/install_node.sh
@@ -201,4 +201,14 @@ sudo sed -i "s|"FXllNVlkD8ADVjFr46teIGRaeWEZXsYVQRMdfmu+UmRV4aysZ30E/OkNadysLZsA
 sudo docker exec --env-file ei.env -it plinode /bin/bash -c ". ~/.profile && pm2 start /pluginAdm/startEI.sh"
 
 
+echo -e "\n\n################# Creating service for automatic startup after reboot #################\n\n"
+
+cp /root/plugin-deployment/oneClickDeploy/nodeboot.sh /usr/local/sbin/
+chmod +x /usr/local/sbin/nodeboot.sh
+cp /root/plugin-deployment/oneClickDeploy/nodeboot.service /etc/systemd/system/nodeboot.service
+chmod +x /etc/systemd/system/nodeboot.service
+systemctl enable nodeboot.service
+systemctl daemon-reload
+
+echo -e "\n\n################# Done #################\n\n"
 echo -e "\n\n################# Node Setup completed. Oracle Deployment Part has to be done manually. Please see: https://docs.goplugin.co for further information #################\n\n"
