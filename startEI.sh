b382e8cc1a696ea1c0b717d81af12a61f052e777
nmzn <95173225+nmzn@users.noreply.github.com>
2022-04-12 07:16:01 +0200

    plirpc updated
    https://plirpc.blocksscan.io/ added, instead of pluginrpc...

--- startEI.sh
+++ startEI.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo '<------------------ STARTING EXTERNAL INITIATOR--------------------------->'
-external-initiator "{\"name\":\"pluginei\",\"type\":\"xinfin\",\"url\":\"https://pluginrpc.blocksscan.io\"}" --chainlinkurl "http://localhost:6688/"
+external-initiator "{\"name\":\"pluginei\",\"type\":\"xinfin\",\"url\":\"https://plirpc.blocksscan.io/"}" --chainlinkurl "http://localhost:6688/"
 echo '<------------------EXTERNAL INITIATOR STARTED--------------------------->'
